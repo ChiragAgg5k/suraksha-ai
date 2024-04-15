deecedd4dbdfafa199a9b1d9e170ead44689bbc3
ChiragAgg5k <110609663+ChiragAgg5k@users.noreply.github.com>
2024-04-15 11:24:54 +0530

    added basic testing

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from spot import app
+
+
+@pytest.fixture()
+def app():
+    app = app()
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
